66f22f44af034953fae08fb666a7a041ca440dd8
muhammad siyyam <siyyam461@gmail.com>
2025-10-16 16:52:54 -0400

    feat(static): add static library libmyutils.a and client_static; update Makefiles for static build

---
+++ src/main_static.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/mystrfunctions.h"
+#include "../include/myfilefunctions.h"
+
+int main(void) {
+    printf("--- client_static: Testing String Functions ---\n");
+    char a[256] = "Static";
+    mystrcat(a, " library");
+    printf("Concatenated: %s (len=%d)\n", a, mystrlen(a));
+
+    char b[32];
+    mystrcpy(b, "xyz");
+    printf("Copied: %s\n", b);
+
+    printf("\n--- client_static: Testing File Functions ---\n");
+    FILE *f = fopen("/etc/hosts", "r");
+    if (!f) { perror("fopen"); return 1; }
+    int lines=0, words=0, chars=0;
+    if (wordCount(f, &lines, &words, &chars) == 0) {
+        printf("/etc/hosts -> lines=%d words=%d chars=%d\n", lines, words, chars);
+    } else {
+        printf("wordCount failed\n");
+    }
+
+    char **matches=NULL;
+    int cnt = mygrep(f, "localhost", &matches);
+    printf("mygrep for 'localhost' returned %d\n", cnt);
+    for (int i=0;i<cnt;i++){
+        printf("match[%d]: %s", i, matches[i]);
+        free(matches[i]);
+    }
+    free(matches);
+    fclose(f);
+    return 0;
+}
