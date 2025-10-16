3cf260f4114be080b5ce787af35b147842b744f7
muhammad siyyam <siyyam461@gmail.com>
2025-10-16 17:43:21 -0400

    fix(myfilefunctions): remove heredoc artefacts; add standard headers and portable implementation

--- src/myfilefunctions.c
+++ src/myfilefunctions.c
@@ -1,7 +1,7 @@
-cat > src/myfilefunctions.c <<'EOF'
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 #include "../include/myfilefunctions.h"
 
 /* portable strdup replacement */
@@ -21,7 +21,6 @@ int wordCount(FILE* file, int* lines, int* words, int* chars) {
     int l = 0, w = 0, c = 0;
     int inword = 0;
 
-    /* rewind to start */
     rewind(file);
     while (fgets(buf, sizeof(buf), file)) {
         l++;
@@ -35,7 +34,6 @@ int wordCount(FILE* file, int* lines, int* words, int* chars) {
             }
         }
     }
-    /* if last token ended without whitespace, count it */
     if (inword) w++;
 
     *lines = l;
@@ -63,7 +61,6 @@ int mygrep(FILE* fp, const char* search_str, char*** matches) {
                 capacity *= 2;
                 char **tmp = realloc(arr, capacity * sizeof(char*));
                 if (!tmp) {
-                    /* cleanup on realloc failure */
                     for (int i = 0; i < count; ++i) free(arr[i]);
                     free(arr);
                     return -1;
@@ -71,7 +68,7 @@ int mygrep(FILE* fp, const char* search_str, char*** matches) {
                 arr = tmp;
             }
             arr[count] = my_strdup(line);
-            if (!arr[count]) { /* cleanup on strdup failure */
+            if (!arr[count]) {
                 for (int i = 0; i < count; ++i) free(arr[i]);
                 free(arr);
                 return -1;
@@ -83,5 +80,3 @@ int mygrep(FILE* fp, const char* search_str, char*** matches) {
     *matches = arr;
     return count;
 }
-EOF
-
